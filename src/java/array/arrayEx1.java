a7846ecc67e075a1e973d7ee393aa8e5e71ebdb0
Sahil <smadankar2004@gamil.com>
2023-08-19 12:08:20 +0530

    assignment.array

---
+++ src/java/array/arrayEx1.java
@@ -0,0 +1,14 @@
+package array;
+
+public class arrayEx1 {
+    public static void main(String[] args) {
+        int [] temp;
+        int[]arr={5,9,15,2,8};
+        int n=arr .length;
+        for (int i=0;i<n;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
